f1e8055bf69a66a160ee2b41d82c592aecae3cb3
socheatos <chansocheatasea@gmail.com>
2022-05-07 16:28:28 -0400

    custom sort and reorder stops at each route

--- helper.py
+++ helper.py
@@ -3,16 +3,19 @@ def addDict(key,value,dictionary={}):
         dictionary[key].append(value)
     else:
         dictionary[key] = [value]
-    
-def sort_sequence_id(d):
-    for v in d.values():
-        v.sort()
-    a = sorted(d.items())      
-    ans = {}
-    for i in range(len(a)):
-        k = a[i][0]
+
+def getSequence(object):
+    return object.stop_sequence
+
+def sort(routes):
+    new = {}
+    # print("sort_d")
+    for k,vs in routes.items():
+        new_val = sorted(vs, key= getSequence)
+        new[k] = new_val
+    for k,vs in new.items():
         count = 0
-        for stop in a[i][1]:
-            addDict(k,stop,ans)
+        for v in vs:
+            v.setStopSeq(count)
             count+=1
-    return ans
+    return new
