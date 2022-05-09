2734ba3ebeb34c3e504a9570b75f0fd639e2b78c
socheatos <chansocheatasea@gmail.com>
2022-05-09 19:13:19 -0400

    added functions to support search.py

--- NYCT.py
+++ NYCT.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 import helper 
 
 class Node:
-    def __init__(self, stop_id, stop_name, stop_sequence, route_id, transit_type,express,transfers,stop_lat, stop_lon, accessibility, notes,precinct,parent=None, child=None):
+    def __init__(self, stop_id, stop_name, stop_sequence, route_id, transit_type,express,transfers,stop_lat, stop_lon, accessibility, notes=None,precinct=None,parent=None, child=None):
         self.stop_id = stop_id
         self.stop_name = stop_name
         self.stop_sequence = stop_sequence
@@ -14,29 +14,74 @@ class Node:
         self.transfers =transfers
         self.transfers_id = deepcopy(transfers)
 
-        self.geocode = [stop_lat, stop_lon]
+        self.geocode = (stop_lat, stop_lon)
         self.accessibility = accessibility
         self.accessibility_notes = notes
         self.precinct = precinct
         
-        
         self.parent = parent
         self.child = child
-    
+
+        self.heuristic_score = None
+
+    def setHeuristicScore(self, value):
+        self.heuristic_score = value
+
+    def isSame(self, other):      
+        # two stops are the same if they have the same transfers or you can transfer bewteen them
+        same_trans = False
+        
+        if self.stop_id == other.stop_id:
+            same_trans = True
+        if self.transfers and other.transfers:
+            if (self.stop_id in other.transfers_id) and (other.stop_id in self.transfers_id):
+                this_transfers = deepcopy(self.transfers_id)
+                other_transfers = deepcopy(other.transfers_id)
+                
+                if (this_transfers == other_transfers) or (this_transfers.remove(other.stop_id) == other_transfers.remove(self.stop_id)):
+                    same_trans = True
+                    
+            
+        return same_trans
+
+    def getPath(self,origin):
+        # need to consider different routes
+        node, hist = self, []
+        same = False
+        while node:
+            if same:
+                break
+            same = node.isSame(origin)
+            # print(node.stop_id, origin.stop_id)
+            hist.append(node)
+            node = node.parent
+            
+        hist.reverse()
+        solution = [node.stop_name for node in hist]
+        return solution
+
+    def __lt__(self, other):
+        # priority queue comparison
+        # compares the heuristic between the two stops
+        return self.heuristic_score < other.heuristic_score
     def __repr__(self):
         if not self.child:
             child = 'NA'
+            child_id = 'NA'
         else: 
             child = self.child.stop_name
+            child_id = self.child.stop_id
         if not self.parent:
             par = 'NA'
+            par_id = 'NA'
         else:
             par = self.parent.stop_name
+            par_id = self.parent.stop_id
         exp = 'LOCAL'
         if self.express==1:
             exp= 'EXPRESS'
         # return f'StopID: {self.stop_id}-{self.stop_name} \nRoute: {self.route_id} Seq:{self.stop_sequence} Transfers: {transfers} \nPrev: {par} \t Next: {child} \nOther: {self.accessibility}, {self.precinct}  \n\n'
-        return f'StopID: {self.stop_id}-{self.stop_name} \nRoute: {self.route_id} {exp} Seq:{self.stop_sequence} Transfers: {self.transfers_id} \nPrev: {par} \t Next: {child} \nOther: {self.accessibility}, {self.precinct}  \n\n'
+        return f'StopID: {self.stop_id}-{self.stop_name} \nRoute: {self.route_id} {exp} Seq:{self.stop_sequence} Transfers: {self.transfers_id} \nPrev: {par}-{par_id} \t Next: {child}-{child_id} \nOther: {self.accessibility}, {self.precinct}  \n\n'
 
     
 
@@ -160,3 +205,10 @@ class Graph:
 # g.generateRoutes('subway',0)
 # g.mapStopTransfers()
 # g.connectStops() 
+
+# g = Graph()
+# a = g.getStop('Canal St')
+# b = g.getStop('168 St-Washington Hts')
+# print(a)
+# print(b)
+# # print(a.child)
