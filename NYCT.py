af0f1c1012720a732804df8d17d185b7f0803b32
socheatos <chansocheatasea@gmail.com>
2022-05-11 20:10:14 -0400

    added new attribute called 'line' to aid with heuristic calculation

--- NYCT.py
+++ NYCT.py
@@ -9,6 +9,7 @@ class Node:
         self.stop_sequence = stop_sequence
         
         self.route_id = route_id
+        self.line = self.setLine()
         self.transit_type = transit_type
         self.express = express
         self.transfers =transfers
@@ -23,6 +24,15 @@ class Node:
         self.child = child
 
         self.heuristic_score = None
+
+    def setLine(self):
+        # maps route_id to which line it is in
+        possible_lines = ['123','456','7','ACE','BDFM','NQRW','JZ','L','GS']
+        for i in range(len(possible_lines)):
+            for j in possible_lines[i]:
+                if self.route_id == j:
+                    return possible_lines[i]
+            
 
     def setHeuristicScore(self, value):
         self.heuristic_score = value
@@ -44,28 +54,31 @@ class Node:
             
         return same_trans
 
-    def getPath(self,origin):
-        # need to consider different routes
-        node, hist = self, []
-        same = False
-        while node:
-            if same:
-                break
-            same = node.isSame(origin)
-            # print(node.stop_id, origin.stop_id)
-            hist.append(node)
-            node = node.parent
+    # def getPath(self,origin):
+    #     # need to consider different routes
+    #     node, hist = self, []
+    #     same = False
+    #     while node:
+    #         if same:
+    #             break
+    #         same = node.isSame(origin)
+    #         # print(node.stop_id, origin.stop_id)
+    #         hist.append(node)
+    #         node = node.parent
             
-        hist.reverse()
-        solution = [node.stop_name for node in hist]
-        return solution
+    #     hist.reverse()
+    #     solution = [node.stop_name for node in hist]
+    #     return solution
 
     def __lt__(self, other):
         # priority queue comparison
         # compares the heuristic between the two stops
+        # TODO maybe can modify to check crime cluster!!
+        
         return self.heuristic_score < other.heuristic_score
 
     def __repr__(self):
+        # string representation
         if not self.child:
             child = 'NA'
             child_id = 'NA'
@@ -81,8 +94,8 @@ class Node:
         exp = 'LOCAL'
         if self.express==1:
             exp= 'EXPRESS'
-        # return f'StopID: {self.stop_id}-{self.stop_name} \nRoute: {self.route_id} Seq:{self.stop_sequence} Transfers: {transfers} \nPrev: {par} \t Next: {child} \nOther: {self.accessibility}, {self.precinct}  \n\n'
-        return f'StopID: {self.stop_id}-{self.stop_name} \nRoute: {self.route_id} {exp} Seq:{self.stop_sequence} Transfers: {self.transfers_id} \nPrev: {par}-{par_id} \t Next: {child}-{child_id} \nOther: {self.accessibility}, {self.precinct}  \n\n'
+       
+        return f'StopID: {self.stop_id}-{self.stop_name} \nRoute: {self.route_id}-{self.line} {exp} Seq:{self.stop_sequence} Transfers: {self.transfers_id} \nPrev: {par}-{par_id} \t Next: {child}-{child_id} \nOther: {self.accessibility}, {self.precinct}  \n\n'
 
     
 
@@ -93,27 +106,28 @@ class Node:
    
 ####################################################
 subways = pd.read_csv(r'datasets\subways_oneD.csv', index_col=False)
-bus = pd.read_csv(r'datasets\bus.csv', index_col=False)
+bus = pd.read_csv(r'datasets\bus_oneD.csv', index_col=False)
+bus_transfers = r'datasets\bus_transfers_oneD.txt'
 subways_transfers = r'datasets\transfers_oneD.txt'
 
 class Graph:
-    def __init__(self):
+    def __init__(self, mode='subway'):
         self.transfers = {}
         self.stopIDs = {}
         self.uptown = {}
         self.downtown = {}
         self.routes = {}
 
-        self.generateTransfers()
-        self.generateStops('subway',0)
+        self.generateTransfers(mode)
+        self.generateStops(mode)
         self.generateStopNames()
-        self.generateRoutes('subway',0)
+        self.generateRoutes(mode)
         self.mapStopTransfers()
         self.connectStops() 
 
 
-
-    def generateStops(self,mode,dir:int):   # generate stop as Node class based on its stopID
+    def generateStops(self,mode):  
+        # generate stop as Node class based on its stopID
         m = bus if mode=='bus' else subways
         for i in range(len(m)):
             line = m.iloc[i]
@@ -131,32 +145,38 @@ class Graph:
             else:
                 _accessibility = line['ADA'] 
                 _notes = line['ADA Notes']
-            if line['direction_id'] == dir: 
-                if str(_stop_id) in self.transfers.keys():
-                    transfers = self.transfers[str(_stop_id)]
-                else: 
-                    transfers=None
-                stop =  Node(_stop_id, _stop_name, _stop_seq, _route_id, 
-                            mode,_express,transfers,
-                            _stop_lat, _stop_lon,
-                            _accessibility, _notes,_precinct)
+        
+            if str(_stop_id) in self.transfers.keys():
+                transfers = self.transfers[str(_stop_id)]
+            else: 
+                transfers=None
+            stop =  Node(_stop_id, _stop_name, _stop_seq, _route_id, 
+                        mode,_express,transfers,
+                        _stop_lat, _stop_lon,
+                        _accessibility, _notes,_precinct)
 
-                self.stopIDs[_stop_id] = stop                     
+            self.stopIDs[_stop_id] = stop                     
                   
 
-    def generateRoutes(self,mode,dir:int):
+    def generateRoutes(self,mode):
+        # creates a dictionary where the keys are the routes and
+        # the values are the stops along that route
+        # the values are ordered by the sequence id
         m = bus if mode=='bus' else subways
         routes ={}
         for i in range(len(m)):
             line = m.iloc[i]
             route_id = line['route_id']
             stop_id = str(line['stop_id'])
-            if line['direction_id'] == dir: 
-                if self.stopIDs[stop_id].route_id == route_id:
-                    helper.addDict(route_id, self.stopIDs[stop_id], routes)     
+            if self.stopIDs[stop_id].route_id == route_id:
+                helper.addDict(route_id, self.stopIDs[stop_id], routes)    
+        
+        # the values are ordered by the sequence id
         self.routes = helper.sort(routes)
 
     def connectStops(self):
+        # links the stops with the previous and upcoming stop
+        # along that particular route
         for route_id, stations in self.routes.items():
             for i in range(len(stations)):
                 curr_node = stations[i]
@@ -170,9 +190,11 @@ class Graph:
                     curr_node.parent = stations[i-1]
                     curr_node.child = stations[i+1]
     
-    def generateTransfers(self):
-        # for subways only... right now
-        with open(subways_transfers) as file:
+    def generateTransfers(self,mode):
+        # creates a dictionary between stop_ids where you 
+        # can change/transfer from
+        m = bus_transfers if mode=='bus' else subways_transfers
+        with open(m) as file:
             for line in file:
                 key= line.rstrip().split(',')
                 if len(key) != 1:
@@ -180,6 +202,7 @@ class Graph:
         
 
     def mapStopTransfers(self):
+        # maps the nodes to its corresponding stopId in the dictionary
         for id, node in self.stopIDs.items():
             if id in self.transfers.keys():
                 transfers = self.transfers[id]
@@ -193,9 +216,11 @@ class Graph:
         for id, stop in self.stopIDs.items():
             helper.addDict(stop.stop_name, stop,self.stopNames)
 
-    def getStop(self, stop):
+    def getStop(self, stop, id=None):
         try:
             return self.stopNames[stop]
         except:
             return self.stopIDs[stop]
 
+# g = Graph()
+# print(g.getStop('0'))
