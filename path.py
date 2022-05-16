20ad4680b8752627da9d14129596973b7875ec2d
socheatos <chansocheatasea@gmail.com>
2022-05-16 12:20:55 -0400

    remove transfer counts. ensure all stops are added to final path

--- path.py
+++ path.py
@@ -1,43 +1,44 @@
-from typing import final
 from NYCT import Node
 
 class Path():
-    def __init__(self, curr_stop:Node, prev_stop= None, n_transfers_route=0,n_transfers_mode=0):
+    def __init__(self, curr_stop:Node, prev_stop= None):
         self.curr_stop = curr_stop
         self.prev_stop = prev_stop
 
-        self.n_transfer_route = prev_stop.n_transfer_route if prev_stop else 0
-        self.n_transfer_mode = prev_stop.n_transfer_mode if prev_stop else 0
-
-    def updateTransfers(self):
-        if self.prev_stop:
-            if self.curr_stop.transit_type != self.prev_stop.curr_stop.transit_type:
-                self.n_transfer_mode +=1
-
     def observePath(self, solution):
+        '''Keeps only the two end stops if you have to transfer between multiple stops in the same stations '''
         final_path = [solution[0]]
         to_add = None
         for i in range(1,len(solution)-1):
             start = final_path[-1]
             end = solution[i]
 
-            if not Node.isSame(start, end):
+            if not start.isSame(end):
                 if to_add:
                     final_path.append(to_add)
                 final_path.append(end)
                 to_add = None
             else:
-                to_add = end
-            
+                to_add = end   
+
+        if final_path[0].isSame(final_path[1]):
+            final_path.pop(0)
+        
+        if solution[-1] not in final_path:
+            final_path.append(solution[-1])
         return final_path
 
+    
     def getPath(self):
         node, history = self,[]
         while node:
-            history.append(node)
+            history.append(node.curr_stop)
             node = node.prev_stop
         
         history.reverse()
-        solution = [node.curr_stop for node in history[1:]]
+        solution = [node for node in history]
+        if self.curr_stop not in solution:
+            solution.append(self.curr_stop)
 
         return self.observePath(solution)
+        # return solution
