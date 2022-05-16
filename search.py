2f405f1b15dd59c32f21fa81d6f131c8eeef490c
sabinab00 <76078849+sabinab00@users.noreply.github.com>
2022-05-16 12:29:30 -0400

    Update search.py

--- search.py
+++ search.py
@@ -4,6 +4,107 @@ from queue import PriorityQueue
 import API_functions
 from path import Path
 
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from sklearn.cluster import KMeans
+
+    
+#checks distance from crime cluster center to given geocode
+    
+#clusters based on longitude and latitude
+crime = pd.read_csv('NYPD dataset.csv')
+
+    #preprocessing 
+crime = crime[crime.BORO_NM != 'BRONX']
+crime = crime[crime.BORO_NM != 'BROOKLYN']
+crime = crime[crime.BORO_NM != 'QUEENS']
+crime = crime[crime.BORO_NM != 'STATEN ISLAND']
+crime = crime.dropna()
+
+    #rename to geocode
+crime = crime.rename(columns = {'Lat_Lon':'Geocode'})
+#crime    
+    
+#plot of crime count by lat, long 
+plt.scatter(crime['Longitude'],crime['Latitude'])
+
+plt.xlim(-74.04,-73.9)
+plt.ylim(40.65,40.9)
+plt.show()
+
+x = crime.iloc[:,8:10] # 1t for rows and second for columns
+
+# fitting kmeans to predict for dataset
+kmeans = KMeans(5)
+kmeans.fit(x)
+
+clusters = kmeans.fit_predict(x)
+clusters
+
+wcss=[]
+for i in range(1,10):
+    kmeans = KMeans(i)
+    kmeans.fit(x)
+    wcss_iter = kmeans.inertia_
+    wcss.append(wcss_iter)
+
+#determine # of clusters using elbow method
+from kneed import KneeLocator
+k1 = KneeLocator(range(1, 10), wcss, curve="convex", direction="decreasing")
+print("number of clusters: ", k1.elbow)
+
+#plot of clusters using k = 3 as # of clusters
+kmeans = KMeans(3)
+kmeans.fit(x)
+clusters = kmeans.fit_predict(x)
+
+data_with_clusters = crime.copy()
+data_with_clusters['Clusters'] = clusters 
+plt.scatter(data_with_clusters['Longitude'],data_with_clusters['Latitude'],c=data_with_clusters['Clusters'],cmap='autumn')
+
+#count per cluster
+from collections import Counter
+
+cluster_labels = kmeans.fit_predict(x)
+cluster_counts = Counter(cluster_labels)
+
+print("count per cluster", cluster_counts)
+
+#prediction
+'''from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters = 3)
+kmeans.fit(x)
+y_kmeans = kmeans.predict(x)
+
+plt.scatter(data_with_clusters['Longitude'],data_with_clusters['Latitude'], c=y_kmeans, cmap='viridis')
+'''
+#find center of each cluster
+
+centers = kmeans.cluster_centers_
+print ("centers: ", centers)
+#plt.scatter(centers[:, 0], centers[:, 1], c='black', alpha=0.5);
+
+center_1 = centers[0]
+center_2 = centers[1]
+center_3 = centers[2]
+distance(center_1, center_2, center_3, destination)
+
+def distance(crime_loc_1: tuple, crime_loc_2: tuple, crime_loc_3:tuple, destination:tuple):
+    '''
+    takes geocode of crime cluster centers and destination to 
+    determine the distance between the two points for each center
+    '''
+    
+    distance_1 = API_functions.get_distance([crime_loc_1], [destination])
+    distance_2 = API_functions.get_distance([crime_loc_2], [destination])
+    distance_3 = API_functions.get_distance([crime_loc_3], [destination])
+
+    return distance_1, distance_2, distance_3
+
 
 def distance(curr_stop: tuple, destination:tuple):
     '''
