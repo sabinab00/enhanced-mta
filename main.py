7cc21b141df560f263f3fc728899d5adef18060b
socheatos <chansocheatasea@gmail.com>
2022-05-16 12:17:02 -0400

    ensures stops are found, there are more than three paths found, paths are sorted

--- main.py
+++ main.py
@@ -2,30 +2,54 @@ from NYCT import Graph, Node
 from queue import PriorityQueue
 from search import ast
 import findendpoints as fep
+import helper
 
 
 if __name__=='__main__':
     g = Graph()
-    # origin = g.getStop('B1')
-    # destination = b = g.getStop('S201')
-    # print(destination.transfers)
-    # ast(origin, destination)
 
     #origin point and destination point
-    access = "y"
-    start = "4 South St Space 2, New York, NY 10004"
-    end = "1220 5th Ave, New York, NY 10029"
-    rad = 0.002
-    f = fep.findroutes(access,mode='subway',origin=start,destination=end,graph=g,rad=rad)
+    access = "n"
+    start = "65 Bayard St, New York, NY 10013"
+    end = "253 W 125th St, New York, NY 10027"
 
-    # prints notes
-    # for t in f:
-    #     print(f"{t[0][0].stop_name} to {t[0][1].stop_name} : {t[0][0].route_id} to {t[0][1].route_id}")
-    #     print(f"{t[0][0].stop_name} to {t[0][1].stop_name} and {t[1]}")
-    # print(f[0])
-    for t in f:
-        print(t[0][0],t[0][1])
-        ast(t[0][0],t[0][1], accessibility=access)
-        print('------------------')
-        # print(f[0])
-    print(len(f))
+    found_stops = False
+    found_path = False
+    rad = 0.002
+    paths_found = []
+
+    while not found_stops or not found_path:
+        # finds the starting and ending points within radius = rad of the start/end user inputs 
+        f = (fep.findroutes(access,mode='all',origin=start,destination=end,graph=g,rad=rad))
+        
+        if len(f)>=1:
+            found_stops = True
+            print('Finding best routes for you...')
+            for t in f:
+                g.restartTransferCount()
+                # a* search here
+                sol = ast(t[0][0],t[0][1], accessibility=access)
+                if sol:
+                    
+                    paths_found.append(sol)
+                    found_path = True
+                                
+        rad += 0.0005
+        if len(paths_found)<4:
+            # ensures we have three or more paths to sort and pick from
+            found_path=False
+            found_stops=False
+
+    # sorts the paths found by
+    #   (1) number of modal transfers
+    #   (2) number of stops passed through
+    #   (3) number of route transfers
+    
+    sorted_paths  = helper.findTopPath(paths_found)
+    for path in sorted_paths[:3]:
+        print('------------')
+        p = path[0]
+        print(len(p), path[1], path[2])
+        print(p)
+    # print(len(paths_found))
+
